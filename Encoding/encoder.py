70a2dad756779287ee771d8df1bdbde361aa2c22
BenjaminTowle <71493502+BenjaminTowle@users.noreply.github.com>
2021-01-30 15:55:13 +0000

    Update and rename encoding.py to Encoding/encoder.py

--- Encoding/encoder.py
+++ Encoding/encoder.py
@@ -1,14 +1,28 @@
 import tensorflow as tf
 import sentencepiece as spm
+import requests
+import tarfile
 
 # This file loads the lite version of Universal Sentence Encoder
 # the model can be downloaded from https://tfhub.dev/google/universal-sentence-encoder-lite/2?tf-hub-format=compressed
 
 try:
-    model = tf.saved_model.load("use-lite3")
+    model = tf.saved_model.load("use-lite")
 except OSError:
-    print("Model must first be downloaded from: https://tfhub.dev/google/universal-sentence-encoder-lite/2?tf-hub-format=compressed")
-
+    url = "https://tfhub.dev/google/universal-sentence-encoder-lite/2?tf-hub-format=compressed"
+    print(f"Downloading model from: {url}")
+    request = requests.get(url, stream=True)
+    save_path = "universal_sentence_encoder.tar.gz"
+    with open(save_path, "wb") as fd:
+        for chunk in request.iter_content(chunk_size=128):
+            fd.write(chunk)
+    # Un-tar file
+    new_path = "use-lite"
+    tar = tarfile.open(save_path)
+    tar.extractall(new_path)
+    tar.close()
+    model = tf.saved_model.load("use-lite")
+    print("Download successful")
 
 
 inference = model.signatures["default"]
@@ -23,9 +37,10 @@ def process_to_IDs_in_sparse_format(sp, sentences):
     # (values, indices, dense_shape)
     ids = [sp.EncodeAsIds(x) for x in sentences]
     max_len = max(len(x) for x in ids)
-    dense_shape=(len(ids), max_len)
-    values=[item for sublist in ids for item in sublist]
-    indices=[[row,col] for row in range(len(ids)) for col in range(len(ids[row]))]
+    dense_shape = (len(ids), max_len)
+    values = [item for sublist in ids for item in sublist]
+    indices = [[row, col] for row in range(len(ids)) for col in range(len(ids[row]))]
+
     return values, indices, dense_shape
 
 
