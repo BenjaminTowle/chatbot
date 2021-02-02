e755c1abcb27e7a7de660f45b51b3debe5aa4300
BenjaminTowle <71493502+BenjaminTowle@users.noreply.github.com>
2021-02-02 20:52:10 +0000

    Update index.py

--- Retrieval/index.py
+++ Retrieval/index.py
@@ -1,38 +1,48 @@
+import numpy as np
 import pickle
 import faiss
 from Retrieval.retriever import Retriever
 from Retrieval.train import EMBEDDING_DIM
 from Datasets.dataloader import load_dataset
+from Encoding.encoder import encode
 
 #####################################
 # INDEX HYPER-PARAMETERS
 #####################################
 N_CLUSTERS = 10
-BATCH_SIZE = 200
-INDEX_PATH = "vectors.index"
+BATCH_SIZE = 100
+RESPONSE_INDEX_PATH = "response_vectors.index"
+CONTEXT_INDEX_PATH = "context_vectors.index"
 
 retriever = Retriever(EMBEDDING_DIM)
 
 
-# Indexes the Empathetic Dialogues dataset - this contains much higher quality responses than the Reddit dataset
-# and they are usually more broadly applicable to a range of context sentences
+# Indexes the Empathetic Dialogues dataset according to embedding of context for context-context matching, as well as
+# mapping of response for context-response matching
 def create_index():
-    quantiser = faiss.IndexFlatIP(EMBEDDING_DIM)
-    index = faiss.IndexIVFFlat(quantiser, EMBEDDING_DIM, N_CLUSTERS, faiss.METRIC_INNER_PRODUCT)
-    utterances = []
+    context_quantiser = faiss.IndexFlatL2(EMBEDDING_DIM)
+    context_index = faiss.IndexIVFFlat(context_quantiser, EMBEDDING_DIM, N_CLUSTERS, faiss.METRIC_INNER_PRODUCT)
+    response_quantiser = faiss.IndexFlatL2(EMBEDDING_DIM)
+    response_index = faiss.IndexIVFFlat(response_quantiser, EMBEDDING_DIM, N_CLUSTERS, faiss.METRIC_INNER_PRODUCT)
 
-    _, responses = load_dataset()
-    utterances += responses
+    contexts, responses = load_dataset(["ed"])
 
-    for i in range(0, len(responses)-BATCH_SIZE, BATCH_SIZE):
+    for i in range(0, len(responses) - BATCH_SIZE, BATCH_SIZE):
         print(i)
-        batch_responses = responses[i:i+BATCH_SIZE]
+        batch_contexts = contexts[i:i + BATCH_SIZE]
+        batch_responses = responses[i:i + BATCH_SIZE]
 
-        # embed responses
-        outputs_norm = retriever.encode_responses(batch_responses)
+        # embed contexts and responses
+        contexts_encoded = encode(batch_contexts)
+        responses_encoded = retriever.encode_responses(batch_responses)
 
-        index.train(outputs_norm)
-        index.add(outputs_norm)
+        context_index.train(contexts_encoded)
+        context_index.add(contexts_encoded)
+        response_index.train(responses_encoded)
+        response_index.add(responses_encoded)
 
-    faiss.write_index(index, INDEX_PATH)
-    pickle.dump(utterances, open("utterances.pickle", "wb"))
+    faiss.write_index(context_index, CONTEXT_INDEX_PATH)
+    pickle.dump(contexts, open("contexts.pickle", "wb"))
+
+    faiss.write_index(response_index, RESPONSE_INDEX_PATH)
+    pickle.dump(responses, open("responses.pickle", "wb"))
